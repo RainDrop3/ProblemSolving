d58a9d726a2c0df2c9358dbac373c806df283bf9
kang <kanghy6220@gmail.com>
2026-01-08 00:42:05 +0900

    DP

---
+++ BOJ/2193.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    long long dp[91], n;
+    cin >> n;
+
+    dp[1] = 1;
+    dp[2] = 1;
+    for(int i=3; i<=n; ++i)
+        dp[i] = dp[i-1] + dp[i-2];
+    
+    cout << dp[n];
+}
